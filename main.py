c767f4c8276a1c5cc92c1439964844617f3c4bd9
drcapitalmanagement300-jpg <drcapitalmanagement300@gmail.com>
2026-01-02 23:39:59 +0100

    Add backtesting page to navigation

--- main.py
+++ main.py
@@ -5,10 +5,11 @@ import streamlit as st
 dashboard = st.Page("streamlit_app.py", title="Dashboard", default=True) 
 spreadfinder = st.Page("spread_finder.py", title="Spread Finder")
 logreview = st.Page("log_review.py", title="Log Review")
+backtesting = st.Page("backtesting.py", title="Back Testing")
 
 # 2. Define Navigation
 # Order here determines the sidebar order
-pg = st.navigation([spreadfinder, dashboard, logreview])
+pg = st.navigation([spreadfinder, dashboard, logreview, backtesting])
 
 # 3. Run the app
 pg.run()
