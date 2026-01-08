2e702f7cfad29ccd1407eb2d2a60c5acda612abd
drcapitalmanagement300-jpg <drcapitalmanagement300@gmail.com>
2026-01-08 23:52:23 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,13 +5,15 @@ import streamlit as st
 dashboard = st.Page("streamlit_app.py", title="Dashboard", default=True) 
 spreadfinder = st.Page("spread_finder.py", title="Spread Finder")
 logreview = st.Page("log_review.py", title="Log Review")
+cornwall = st.Page("cornwall_hunter.py", title="Cornwall Hunter") 
 #backtesting = st.Page("backtesting.py", title="Options Lab")
 
 # 2. Define Navigation
 # Order here determines the sidebar order
-pg = st.navigation([spreadfinder, dashboard, logreview])
-#Unhash bellow to enable backtesting.
-#pg = st.navigation([spreadfinder, dashboard, logreview, backtesting])
+pg = st.navigation([spreadfinder, dashboard, logreview, cornwall])
+
+# Unhash below to enable backtesting.
+#pg = st.navigation([spreadfinder, dashboard, logreview, cornwall, backtesting])
 
 # 3. Run the app
 pg.run()
