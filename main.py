63a9f0d669d7f830c8f7624efb07f18d2dbfa711
drcapitalmanagement300-jpg <drcapitalmanagement300@gmail.com>
2026-01-24 22:39:01 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,18 +2,19 @@ import streamlit as st
 
 # 1. Define your pages
 # The 'default=True' here ensures Dashboard loads first
-dashboard = st.Page("streamlit_app.py", title="Dashboard", default=True) 
-#spreadfinder = st.Page("spread_finder.py", title="Spread Finder")
+dashboard = st.Page("streamlit_app.py", title="Dashboard", default=True)
+macro = st.Page("macro_dashboard.py", title="Macro Command Center") # <--- NEW PAGE
+#spreadfinder = st.Page("spread_finder.py", title="Spread Finder") 
 logreview = st.Page("log_review.py", title="Log Review")
 cornwall = st.Page("cornwall_hunter.py", title="Cornwall Hunter") 
 #backtesting = st.Page("backtesting.py", title="Options Lab")
 
 # 2. Define Navigation
 # Order here determines the sidebar order
-pg = st.navigation([dashboard, logreview, cornwall])
+pg = st.navigation([macro, dashboard, logreview, cornwall])
 
-# Unhash below to enable backtesting.
-#pg = st.navigation([spreadfinder, dashboard, logreview, cornwall, backtesting])
+# Unhash below if you want to enable the Spread Finder or Backtesting later
+#pg = st.navigation([dashboard, macro, spreadfinder, logreview, cornwall, backtesting])
 
 # 3. Run the app
 pg.run()
